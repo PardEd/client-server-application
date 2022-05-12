af81bfb0cbcc2414aea95a1ee822311432adab3e
PardEd <edbashirov@gmail.com>
2022-05-12 15:03:30 +0500

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    private static final String name = "Netology";
+    private static final String messFromServ = "Message from Server: ";
+
+    public static void main(String[] args) {
+
+        try {
+            try (Socket clientSocket = new Socket("localhost", 8080);
+                BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter out = new BufferedWriter(
+                    new OutputStreamWriter(clientSocket.getOutputStream()))) {
+
+                System.out.printf("Подключение к серверу с портом: %s ",
+                    clientSocket.getPort() + "\n");
+
+                String word = Integer.toString(clientSocket.getPort());
+                out.write(word + "\n");
+                out.flush();
+
+                String serverWord = in.readLine();
+                System.out.println(messFromServ + serverWord);
+
+                serverWord = in.readLine();
+                System.out.println(messFromServ + serverWord);
+                if (serverWord.equalsIgnoreCase("Write your name")) {
+                    out.write(name + "\n");
+                    out.flush();
+                }
+
+                serverWord = in.readLine();
+                System.out.println(messFromServ + serverWord);
+                if (serverWord.equalsIgnoreCase("Write your name")) {
+                    out.write("My name is Netology" + "\n");
+                    out.flush();
+                }
+                out.write("yes" + "\n");
+                out.flush();
+
+                serverWord = in.readLine();
+                System.out.println(messFromServ + serverWord);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
